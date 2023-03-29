5cf71f02230f95324625c9d604dc8ed9a0877ab9
Antônia Turcatto <lucasturcatto.va048@academico.ifsul.edu.br>
2023-03-28 21:00:00 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    //a)
+    var num1 =1
+    var num2=10
+    println(num1+num2)
+
+    //B)
+    var booleano1 : Boolean = (5 > 3 && 2 < 4)//guardará 'true'
+    println(booleano1)
+
+    //C)
+    var booleano2 : Boolean = (!(10 > 5 || 3 < 1))//guardará 'false'
+    print(booleano2)
+}
